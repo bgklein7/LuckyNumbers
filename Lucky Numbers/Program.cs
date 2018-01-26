67b44ac2b3e0067e4becd2d8f8239f8f9d7cb84d
bgklein7 <bgklein@outlook.com>
2018-01-26 12:45:40 -0500

    initial commit

---
+++ Lucky Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float jackpot = 1000000;
+            Console.WriteLine("Welcome to the $" + jackpot + " Lucky Numbers Game!");
+
+        }
+    }
+}
